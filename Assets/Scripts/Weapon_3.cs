f78b0364634ec225d1080b946ea54f545a20b3eb
松山賢勝 <matsuyama@beyondjapan.com>
2024-03-25 17:55:26 +0900

    武器３（鎌）の使用不可時間を追加

--- Assets/Scripts/Weapon_3.cs
+++ Assets/Scripts/Weapon_3.cs
@@ -10,6 +10,9 @@ public class Weapon_3 : WeaponBase
     // 同時出現数（３レベル）
     private readonly int[] simultaneous = { 1, 2, 3 };
     private readonly float[] relativeAngles = { 0.0f, 180.0f, 120.0f };
+    private readonly int[] hitMaxes = { 5, 15, 30 };
+    private int curHit = 0;
+    private readonly int waitTime = 5;
 
     // 生成済みの鎌
     private List<GameObject> sickles = new List<GameObject>();
@@ -43,6 +46,9 @@ public class Weapon_3 : WeaponBase
             // 初期化
             sickle.GetComponent<Sickle>().Initialize(player, rot);
             rot += relativeAngles[GetLevel()];
+
+            // 当たり判定を監視し、
+            sickle.GetComponent<Sickle>().OnHit.AddListener(() => Hit(sickle));
         }
 
         // 生成済みの鎌の数が生成する鎌の数より多い場合は、削除
@@ -51,8 +57,41 @@ public class Weapon_3 : WeaponBase
             Destroy(sickles[i]);
             sickles.RemoveAt(i);
         }
-
     }
 
+    /// <summary>
+    /// 当たり判定が発生したら呼ばれる
+    /// </summary>
+    /// <param name="sickle"></param>
+    private void Hit(GameObject sickle)
+    {
+        curHit++;
+        if(curHit >= hitMaxes[GetLevel()])
+        {
+            // ヒット数をリセット
+            curHit = 0;
+            // 武器を非アクティブにする
+            for(int i=0 ; i<sickles.Count ; i++)
+            {
+                sickles[i].SetActive(false);
+            }
 
+            // 待機時間後に再度アクティブにする
+            StartCoroutine(WaitActive());
+        }
+    }
+
+    /// <summary>
+    /// 使用不可時間
+    /// </summary>
+    /// <returns>こルーチン</returns>
+    private IEnumerator WaitActive()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        for(int i=0 ; i<sickles.Count ; i++)
+        {
+            sickles[i].SetActive(true);
+        }
+    }
 }
