f4e97843478219e2854aae1a5f658f190c8f7482
松山賢勝 <matsuyama@beyondjapan.com>
2024-05-29 21:25:35 +0900

    マグネットを実装

--- Assets/Scripts/ExpController.cs
+++ Assets/Scripts/ExpController.cs
@@ -7,12 +7,24 @@ using UnityEngine;
 /// </summary>
 public class ExpController : MonoBehaviour
 {
+    public enum Type
+    {
+        Exp,    // 経験値
+        Mag     // マグネット
+    };
+
+    public struct ExpData
+    {
+        public Type         type;
+        public GameObject   obj;
+    };
+
     [SerializeField] private PlayerController playerController;
 
-    [SerializeField] private GameObject prefab;
+    [SerializeField] private GameObject[] prefab;
     [SerializeField] private GameObject parent;
 
-    private List<GameObject> exps = new List<GameObject>();
+    private List<ExpData> exps = new List<ExpData>();
 
     private const float MoveThreshold = 2f;
 
@@ -27,17 +39,52 @@ public class ExpController : MonoBehaviour
     /// <param name="pos">配置座標</param>
     public void Spawn(Vector2 pos)
     {
-        var exp = Instantiate(prefab, pos, Quaternion.identity, parent.transform);
-        exps.Add(exp);
+        // マグネットが既に存在するか確認する
+        bool isExistMag = false;
+        foreach (var exp in exps)
+        {
+            if (exp.type == Type.Mag)
+            {
+                isExistMag = true;
+                break;
+            }
+        }
+
+        // 経験値が50を超えたら確率でマグネットを生成する
+        Type type = Type.Exp;
+        if (expNum >= 50 && !isExistMag)
+        {
+            if (Random.Range(0, 100) < 10)
+            {
+                type = Type.Mag;
+            }
+        }
+
+        // オブジェクト生成
+        var obj = Instantiate(prefab[(int)type], pos, Quaternion.identity, parent.transform);
+        exps.Add(new ExpData { type = type, obj = obj });
     }
 
     /// <summary>
     /// exp オブジェクトを削除する
     /// </summary>
     /// <param name="exp">削除するオブジェクト</param>
-    public void Remove(GameObject exp)
+    public void Remove(ExpData exp)
     {
-        expNum++;
+        if (exp.type == Type.Exp) expNum++; // 経験値を取得
+        else {                              // マグネットの効果
+            // 移動中でない exp オブジェクトをプレイヤーに向けて移動させる
+            foreach (var e in exps)
+            {
+                GameObject obj = e.obj;
+                if (!obj.GetComponent<Exp>().IsMoving)
+                {
+                    pull(e);
+                }
+            }
+        }
+
+        // 破棄
         exps.Remove(exp);
     }
 
@@ -53,18 +100,28 @@ public class ExpController : MonoBehaviour
         foreach (var exp in exps)
         {
             // 移動中でない場合
-            if (!exp.GetComponent<Exp>().IsMoving)
+            GameObject obj = exp.obj;
+            if (!obj.GetComponent<Exp>().IsMoving)
             {
                 // プレイヤーとの距離を計算
-                float distance = Vector2.Distance(playerPos, exp.transform.localPosition);
+                float distance = Vector2.Distance(playerPos, obj.transform.localPosition);
                 if (distance < MoveThreshold)
                 {
-                    exp.GetComponent<Exp>().Move(playerController);
-                    // 移動終了したらリストから削除
-                    exp.GetComponent<Exp>().OnDeleted.AddListener(() => Remove(exp));
+                    pull(exp);
                 }
             }
         }
+    }
+
+    /// <summary>
+    /// 引き寄せ処理
+    /// </summary>
+    /// <param name="exp">Exp データ</param>
+    private void pull(ExpData exp)
+    {
+        exp.obj.GetComponent<Exp>().Move(playerController);
+        // 移動終了したらリストから削除
+        exp.obj.GetComponent<Exp>().OnDeleted.AddListener(() => Remove(exp));
     }
 
 }
